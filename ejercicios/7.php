f6fa8e1502f2892a08281e20c19f74d54a0a6ae2
Gabriel <gabriel.hdezcorrea@gmail.com>
2024-10-02 12:52:15 +0200

    2/10 clase

--- ejercicios/7.php
+++ ejercicios/7.php
@@ -3,8 +3,11 @@
 muestre por pantalla. Un número primo es un número entero que sólo es
 divisible por 1 y por sí mismo..*/
 
-for ( $i = 0; $i <= 50; $i=$i+1 ) {
-    if ( $i %2== 0) {
-    echo " | ". $i ;}
-}
+for ($num=2; $num<=50; $num++) {
+	$primo=true;
+    for ($i=2; $i<=$num/2; $i++) {
+      if ($num % $i == 0) $primo=false;
+    }
+    if ($primo==true) echo "$num<br>";
+  }
 ?>
